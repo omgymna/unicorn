19b1a00b84b54bf75439e2d8b921bb1c4d4930d3
omgymna <romojimena244@gmail.com>
2022-11-27 22:42:39 -0600

    adding config details

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId:         '',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             ''          // e.g. us-east-2
+        userPoolId:         'us-east-2_i3MOEvdAP',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   'ctj94v67gaat78du26esse4i1',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region:             'us-east-2'          // e.g. us-east-2
     },
     api: {
         invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
