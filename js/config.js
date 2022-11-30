e9ece9fe33154021e49958f13952580b7c7c993e
omgymna <romojimena244@gmail.com>
2022-11-29 18:13:41 -0600

    resetting

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region:             'us-east-2'          // e.g. us-east-2
     },
     api: {
-        invokeUrl:          'https://jj2t8zjh7a.execute-api.us-east-2.amazonaws.com/prod'          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl:          '',     //https://jj2t8zjh7a.execute-api.us-east-2.amazonaws.com/prod'          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod
     }
 };
